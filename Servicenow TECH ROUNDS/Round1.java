7dc568731092149d8e68097d463455baeeb864d0
Krishna18062005 <krish1826s@gmail.com>
2025-11-12 23:06:47 +0530

    Servicenow

---
+++ Servicenow TECH ROUNDS/Round1.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+     // Scanner sc=new Scanner(System.in);
+      
+      // String input=sc.nextLine();
+      // char[] inpArray=input.toCharArray();
+      // int i=0;
+      // int j=inpArray.length-1;
+      // while(i<=j){
+      //   char temp=inpArray[i];
+      //   inpArray[i]=inpArray[j];
+      //   inpArray[j]=temp;
+      //   i++;
+      //   j--;
+      // }
+      // System.out.println(new String(inpArray));
+      
+      //
+      // int number =1234;
+      // int reversed=0;
+      // while(number>0){
+      //   reversed=reversed*10+(number%10);
+      //   number/=10;
+      // }
+      // System.out.println(reversed);
+      
+      
+      
+      // int a=5;
+      // int b=6;
+      // a=a+b;//11
+      // b=a-b;//11-6=5;
+      // a=a-b;//11-5=6
+      // System.out.print(a+" "+b);
+      
+          // long n=(long)25;
+      // for(int i=7;i>=0;i-=2){
+      //   for(int j=i;j>=0;j--){
+      //     System.out.print(" ");
+      //   }
+      //   for(int k=7-i;k>=0;k--){
+      //     System.out.print("* ");
+      //   }
+      //   System.out.println();
+      // }
+      // int[] array={1,2,3,4,5};
+      // int last=array[array.length-1];
+      // for(int i=array.length-1;i>0;i--){
+      //   array[i]=array[i-1];
+      // }
+      
+      // array[0]=last;
+      // System.out.println(Arrays.toString(array));
+      // String ip="encyclopedia";
+      // char[] ar=ip.toCharArray();
+      
+      // //
+      // HashMap<Character,Integer> map=new HashMap<>();
+      
+      // for(char i:ar){
+      //   if(map.containsKey(i)){
+      //     map.put(i,map.get(i)+1);
+      //   }
+      //   else{
+      //     map.put(i,1);
+      //   }
+      // }
+      // System.out.print(map);
+  }
+  
+}
---
+++ Servicenow TECH ROUNDS/Round2.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Main {
+  
+  
+    public static void main(String[] args) {
+      // String str="This is my Second Interview Round";
+      // String[] arr=str.split(" ");
+      // for(int i=0;i<arr.length;i++){
+      //   for(int j=arr[i].length()-1;j>=0;j--){
+      //     System.out.print(arr[i].charAt(j));
+      //   }
+      //   System.out.print(" ");
+      // }
+      
+      // //System.out.println(reversed);
+      // int a=3;
+      // for(int i=0;i<(2*a)+1;i+=2){
+      //   for(int j=0;j<=(a*2)-i;j++){
+      //     System.out.print(" ");
+      //   }
+      //   for(int k=0;k<=i;k++){
+      //     System.out.print("* ");
+      //   }
+      //   System.out.println();
+      // }
+      // String num=123.456+"";
+      // System.out.print(num);
+      // String st="bananaart";
+      // char ch='a';
+      // HashMap<Character,Integer> map=new HashMap<>();
+      // for(int i=0;i<st.length();i++){
+      //   if(map.containsKey(st.charAt(i))){
+      //     map.put(st.charAt(i),map.get(st.charAt(i))+1);
+      //   }
+      //   else{
+      //     map.put(st.charAt(i),1);
+      //   }
+      // }
+      // System.out.print("a = "+map.get('a'));
+  }
+}
